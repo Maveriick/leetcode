3b5db2df8428419c3a95124dde002ba097014e8c
Ashutosh Kale <ashutoshkale@outlook.com>
2021-07-20 13:50:08 -0700

    Time: 1 ms (99.55%), Space: 39.5 MB (27.41%) - LeetHub

--- subsets-ii/subsets-ii.java
+++ subsets-ii/subsets-ii.java
@@ -1,35 +1,22 @@
 class Solution {
-     List<List<Integer>> solution = new ArrayList<>();
+    List<List<Integer>> sol = new ArrayList<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<Integer> candidatesList = new ArrayList<>();
-        for(int i : nums) {
-            candidatesList.add(i);
-        }
-        List<Integer> current = new ArrayList<>();
-        backtrack(current, candidatesList);
-        return solution;
+        subsets(nums, 0, new ArrayList<>());
+        return sol;
     }
     
-    
-    private void backtrack(List<Integer> current, List<Integer> candidates) {
-       
-        if(!solution.contains(current)){
-             solution.add(new ArrayList<Integer>(current));
-        }
-       
+    private void subsets(int[] nums, int index, List<Integer> currentSet) {
+        sol.add(new ArrayList<Integer>(currentSet));
         
-        for(int i = 0; i < candidates.size(); i++) {
-                current.add(candidates.get(i));
-                List<Integer> remaining = new ArrayList<>();
-
-                remaining.addAll(candidates.subList(i + 1, candidates.size()));
-                backtrack(current, remaining);
-                current.remove(current.size() - 1);
+        for(int i = index; i < nums.length; i++) {
+            if( i > index && nums[i] == nums[i-1]){
+                continue;
+            }
+            currentSet.add(nums[i]);
+            subsets(nums, i + 1, currentSet);
+            currentSet.remove(currentSet.size() - 1);
             
         }
-        
     }
-    
-    
 }
