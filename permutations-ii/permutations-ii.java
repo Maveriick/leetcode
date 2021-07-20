88343ecafd9a29f8bcc20a7b7fb8bd96f47b84e8
Ashutosh Kale <ashutoshkale@outlook.com>
2021-07-20 00:15:05 -0700

    Time: 7 ms (28.62%), Space: 39.6 MB (74.54%) - LeetHub

--- permutations-ii/permutations-ii.java
+++ permutations-ii/permutations-ii.java
@@ -1,28 +1,28 @@
 class Solution {
-   List<List<Integer>> sol = new ArrayList<>();
+     List<List<Integer>> sol = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
-        boolean [] used = new boolean[nums.length];
-        recursive(new ArrayList<>(), nums, used);
+        permute(nums, new ArrayList<>(), new HashSet<>());
         return sol;
     }
     
-     private void recursive(List<Integer> current, int[] nums, boolean [] used){
-        if(current.size() == nums.length){
-            sol.add(new ArrayList<Integer>(current));
+    private void permute(int[] nums, List<Integer> currentList, Set<Integer> seenIndex) {
+        
+        if(currentList.size() == nums.length) {
+            sol.add(new ArrayList<>(currentList));
             return;
         }
         
-        for(int i = 0; i < nums.length; i++){
-                if(used[i] == true || i > 0 && nums[i] == nums[i-1] && used[i - 1] == false) {
-                    continue;
-                }
+        for(int i = 0; i < nums.length; i++) {
+            if(seenIndex.contains(i) || i > 0 && nums[i] == nums[i-1] && seenIndex.contains(i-1) ) continue;
+
+                currentList.add(nums[i]);
+                seenIndex.add(i);
+                permute(nums, currentList, seenIndex);
+                seenIndex.remove(i);
+                currentList.remove(currentList.size() - 1); 
             
-                used[i] = true;
-                current.add(nums[i]);
-                recursive(current, nums, used);
-                current.remove(current.size() - 1);
-                used[i] = false;
+                         
         }
     }
 }
