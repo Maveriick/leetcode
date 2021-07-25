64375c5d3d2fbce96b3eef1f754185c36e85db3c
Ashutosh Kale <ashutoshkale@outlook.com>
2021-07-25 15:05:03 -0700

    Time: 10 ms (97.31%), Space: 38.7 MB (51.15%) - LeetHub

--- coin-change/coin-change.java
+++ coin-change/coin-change.java
@@ -1,23 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0 || coins.length == 0) {
+        if(amount <= 0){
             return 0;
         }
         int[] minCoins = new int[amount + 1];
-        Arrays.fill(minCoins, Integer.MAX_VALUE);
         minCoins[0] = 0;
-       
-        //System.out.println(Arrays.toString(minCoins));
         for(int i = 1; i <= amount; i++) {
+            int minimum = Integer.MAX_VALUE;
             for(int j = 0; j < coins.length; j++) {
-                if(coins[j] <= i && minCoins[i - coins[j]] != Integer.MAX_VALUE){
-                    minCoins[i] = Math.min(minCoins[i], minCoins[i - coins[j]] + 1);
+                if(coins[j] <= i){
+                    minimum = Math.min(minimum, minCoins[i - coins[j]]);
                 }
             }
+            minCoins[i] = minimum == Integer.MAX_VALUE ? Integer.MAX_VALUE : 1 + minimum;
         }
-        //System.out.println(Arrays.toString(minCoins));
-        return minCoins[amount] >= Integer.MAX_VALUE ? - 1 : minCoins[amount];
+        
+        return minCoins[amount] == Integer.MAX_VALUE ? -1 : minCoins[amount];
         
     }
 }
+
 
