4182e6b01db51d0a9ceacd9b20c1c8ecc884e63a
Ashutosh Kale <ashutoshkale@outlook.com>
2021-07-26 19:44:38 -0700

    Time: 51 ms (5.82%), Space: 38.9 MB (5.40%) - LeetHub

--- minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.java
+++ minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.java
@@ -1,17 +1,17 @@
 class Solution {
     public int minOperations(int n) {
-        int moves = 0;
-        int mid = n;
-        for(int i = 1; i < n; i += 2){
-            moves += mid - i;
+        int operations = 0;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = i * 2 + 1;
         }
-        return moves;
+        System.out.println(Arrays.toString(arr));
+         for(int i = 0; i < arr.length; i++) {
+                operations += Math.abs(arr[i] - n);
+            }
+        return operations / 2;
+        
     }
 }
 
-/*
-[1,3,5,7,9,11] 6
-[1,3,5,7,9,11,13] 7
-[1,3,5,7,9,11,13,15] 8 
-*/
 
