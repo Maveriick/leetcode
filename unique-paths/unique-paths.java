bdf33e5332737f04c6d87fad38ceb598d01e75fc
Ashutosh Kale <ashutoshkale@outlook.com>
2021-07-29 09:29:12 -0700

    Time: 0 ms (100.00%), Space: 35.5 MB (88.97%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,30 +1,27 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m ][n ];
-       
-        for(int i = 0; i < n; i++) {
-            dp[0][i] = 1;
+        int[][] dp = new int[m][n];
+        
+        if(m == 1 && n == 1){
+            return 1;
+        }
+        for(int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
         
-        for(int i = 0; i < m; i++){
-            dp[i][0]  = 1;
+        for(int j = 0; j < n; j++) {
+            dp[0][j] = 1;
         }
-         dp[0][0] = 1;
         
+        dp[0][0] = 0;
         
-        int[] xDirs = new int[]{-1, 0};
-        int[] yDirs = new int[]{0,-1};
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                for(int k = 0; k < xDirs.length; k++) {
-                    if(i + xDirs[k] >= 0 && i + xDirs[k] < m && j + yDirs[k] >= 0 && j + yDirs[k] < n) {
-                        dp[i][j] += dp[i + xDirs[k]][j + yDirs[k]];
-                    }
-                }
+        for(int i = 1; i < m ; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
         return dp[m-1][n-1];
+        
     }
 }
