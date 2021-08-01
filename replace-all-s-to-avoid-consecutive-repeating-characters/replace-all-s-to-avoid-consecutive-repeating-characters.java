612fcc8dea24c00e7d1fc13f1e3dd42c4ee2b5f0
Ashutosh Kale <ashutoshkale@outlook.com>
2021-07-31 21:32:17 -0700

    Time: 4 ms (15.06%), Space: 40.5 MB (6.37%) - LeetHub

--- replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.java
+++ replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.java
@@ -1,43 +1,39 @@
 class Solution {
     public String modifyString(String s) {
-        if(s.length() == 0){
-            return s;
-        }
-        
-        
-        if(s.length() == 1){
-            if(s.charAt(0) != '?'){
-                return s;
-            } else {
+        if(s.length() == 1) {
+            if(s.charAt(0) == '?'){
                 return "a";
+            } else {
+                return s;
             }
         }
-        
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '?'){
-                for(char j = 'a' ; j <= 'z'; j++) {
-                    if(i > 0 && i < s.length() - 1) {
-                        if(sb.charAt(i - 1) != j && j != s.charAt(i + 1)){
-                            sb.append(j);
-                            break;
-                        } 
-                    }else if(i == 0){
-                        if(j != s.charAt(i + 1)){
-                            sb.append(j);
+            Character current = s.charAt(i);
+            if(current == '?') {
+                for(char j = 'a'; j <= 'z'; j++) {
+                    if(i == 0 ){
+                        if(s.charAt(i + 1) != j){
+                             sb.append(j);
                             break;
                         }
-                    } else if(i == s.length() - 1) {
-                        if(j != sb.charAt(i - 1)){
+                    } else if( i == s.length() - 1) {
+                        if(sb.charAt(i - 1) != j){
+                             sb.append(j);
+                             break;
+                        }
+                    } else {
+                        if(s.charAt(i + 1) != j && sb.charAt(i-1) != j){
                             sb.append(j);
-                            break;
+                             break;
                         }
                     }
                 }
             } else {
-                sb.append(s.charAt(i));
+                sb.append(current);
             }
         }
         return sb.toString();
+                    
     }
 }
