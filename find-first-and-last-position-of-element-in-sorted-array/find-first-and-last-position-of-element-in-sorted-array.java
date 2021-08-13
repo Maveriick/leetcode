2f471cd09ec19122e1e0c97235776b2762a64c20
Ashutosh Kale <ashutoshkale@outlook.com>
2021-08-12 22:22:53 -0700

    Time: 0 ms (100.00%), Space: 44.2 MB (5.86%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,50 @@
 class Solution {
-    int min = Integer.MAX_VALUE;
-    int max = Integer.MIN_VALUE;
     public int[] searchRange(int[] nums, int target) {
-        binarySearch(nums, 0, nums.length - 1, target);
-        if(min == Integer.MAX_VALUE) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        int first = findPosition(nums, target, true);
+        int last = findPosition(nums, target, false);
+        
+        if(first == -1){
             return new int[]{-1,-1};
-        } else {
-            return new int[]{min,max};
         }
+        
+        return new int[]{first, last};
+        
         
     }
     
-    private void binarySearch(int[] nums, int start, int end, int target){
-        if(start > end){
-            return;
-        }
-        int mid =  start + (end - start) / 2;
-        if(nums[mid] == target){
-            min = Math.min(min, mid);
-            max = Math.max(max, mid);
-            binarySearch(nums, start, mid - 1, target);
-            binarySearch(nums, mid + 1, end, target);
-        } else if(nums[mid] < target){
-            binarySearch(nums, mid + 1, end, target);
-        } else {
-            binarySearch(nums, start, mid - 1, target);
-        }
+    
+    private int findPosition(int[] nums, int target, boolean isFirst) {
+        int start = 0;
+        int end = nums.length - 1;
+        int position = -1;
         
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] < target) {
+                start = mid + 1;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                if(isFirst) {
+                    if(mid == 0 || nums[mid] != nums[mid - 1]){
+                        return mid;
+                    }
+                    
+                    end = mid -1;
+                } else {
+                    if(mid == nums.length - 1 || nums[mid] != nums[mid + 1]){
+                        return mid;
+                    }
+                    
+                    start = mid + 1;
+                }
+            }
+        }
+        return position;
     }
 }
+
+
