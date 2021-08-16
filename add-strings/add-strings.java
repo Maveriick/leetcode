24c5016e81f497f5f60ad4f8b9965af337a1705b
Ashutosh Kale <ashutoshkale@outlook.com>
2021-08-15 17:18:33 -0700

    Time: 4 ms (30.04%), Space: 39.3 MB (30.38%) - LeetHub

--- add-strings/add-strings.java
+++ add-strings/add-strings.java
@@ -1,56 +1,65 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+        
+        StringBuilder sb = new StringBuilder();
         int num1Start = num1.length() - 1;
         int num2Start = num2.length() - 1;
+      
         int carry = 0;
-        StringBuilder resultString = new StringBuilder();
-        while(num1Start >= 0 && num2Start >= 0) {
-            int num1Digit = num1.charAt(num1Start) - '0';
-            int num2Digit = num2.charAt(num2Start) - '0';
-            int result = num1Digit + num2Digit + carry;
-            if(result > 9) {
-                result = result - 10;
+        
+        while(num1Start >=0 && num2Start >= 0){
+            int num1Digit = Integer.parseInt(String.valueOf(num1.charAt(num1Start)));
+            int num2Digit = Integer.parseInt(String.valueOf(num2.charAt(num2Start)));
+            int sum = num1Digit + num2Digit + carry;
+            if(sum > 9){
+                sum = sum - 10;
                 carry = 1;
             } else {
                 carry = 0;
             }
-            resultString.insert(0, Integer.toString(result));
-            num1Start --;
-            num2Start --;
             
+           sb.insert(0, sum);
+            num1Start--;
+            num2Start--;
         }
+        
         
         if(num1Start < 0) {
-            while(num2Start >= 0) {
-                int num2Digit = num2.charAt(num2Start) - '0';
-                int result = carry + num2Digit;
-                if(result > 9) {
-                    result = result - 10;
+            while(num2Start >= 0){
+                int num2Digit = Integer.parseInt(String.valueOf(num2.charAt(num2Start)));
+                 int sum =  num2Digit + carry;
+                if(sum > 9){
+                    sum = sum - 10;
                     carry = 1;
                 } else {
                     carry = 0;
                 }
-                resultString.insert(0, Integer.toString(result));
-                num2Start --;
+                sb.insert(0, sum);
+                num2Start--;
             }
         } else {
-            while(num1Start >= 0) {
-                int num1Digit = num1.charAt(num1Start) - '0';
-                int result = carry + num1Digit;
-                if(result > 9) {
-                    result = result - 10;
+             while(num1Start >= 0){
+                int num1Digit = Integer.parseInt(String.valueOf(num1.charAt(num1Start)));
+                int sum = num1Digit + carry;
+                if(sum > 9){
+                    sum = sum - 10;
                     carry = 1;
                 } else {
                     carry = 0;
                 }
-                resultString.insert(0, Integer.toString(result));
-                num1Start --;
+               sb.insert(0, sum);
+                num1Start--;
             }
         }
-        
-        if(carry == 1){
-            resultString.insert(0, carry);
+        if(carry == 1) {
+            sb.insert(0, carry);
         }
-        return resultString.toString();
+        
+        
+        return sb.toString();
+     
+        
+        
+        
     }
 }
