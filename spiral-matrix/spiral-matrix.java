723223cd9c4756ada71cf64828e212a9270b63df
Ashutosh Kale <ashutoshkale@outlook.com>
2021-08-27 23:26:15 -0700

    Time: 0 ms (100.00%), Space: 36.8 MB (93.41%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,68 +1,63 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int rowStart = 0;
-        int rowEnd = matrix.length - 1;
-        int colStart = 0;
-        int colEnd = matrix[0].length - 1;
-        int numElements = matrix.length * matrix[0].length;
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+        
+        
+        int totalElements = matrix.length * matrix[0].length;
         int elements = 0;
         List<Integer> sol = new ArrayList<>();
-        while(elements < numElements) {
-          
-            for(int i = colStart; i <= colEnd; i++) {
-                sol.add(matrix[rowStart][i]);
+        while(elements < totalElements) {
+            
+            // Top Row
+            for(int i = startCol; i <= endCol; i++ ) {
+                sol.add(matrix[startRow][i]);
                 elements++;
             }
-            rowStart ++;
-            System.out.println(elements);
-            System.out.println("Num" + elements);
-            if(elements == numElements) {
+            if(elements == totalElements) {
                 break;
             }
             
-           
+            startRow++;
             
-            for(int i = rowStart; i <= rowEnd; i++){
-                sol.add(matrix[i][colEnd]);
+            
+            // Right Col
+            for(int i = startRow; i <= endRow; i++ ) {
+                sol.add(matrix[i][endCol]);
                 elements++;
             }
-            colEnd --;
-            System.out.println(elements);
-             if(elements == numElements) {
-                break;
-            }
-           
-            
-            for(int i = colEnd; i >= colStart; i--) {
-                sol.add(matrix[rowEnd][i]);
-                elements++;
-            }
-            rowEnd --;
-            System.out.println(elements);
-             if(elements == numElements) {
+            if(elements == totalElements) {
                 break;
             }
             
+            endCol--;
             
-            for(int i = rowEnd; i >= rowStart; i--){
-                sol.add(matrix[i][colStart]);
+            
+            
+            // Bottom Row
+            for(int i = endCol; i >= startCol; i-- ) {
+                sol.add(matrix[endRow][i]);
                 elements++;
             }
-            colStart++;
-            System.out.println(elements);
-             if(elements == numElements) {
+            if(elements == totalElements) {
                 break;
             }
+            endRow --;
             
+            
+            /// Left Col
+            for(int i = endRow; i >= startRow; i-- ) {
+                sol.add(matrix[i][startCol]);
+                elements++;
+            }
+            if(elements == totalElements) {
+                break;
+            }
+            startCol++;
         }
-        
         return sol;
+        
     }
 }
-
-
-/*
-1 2 3 4 0
-5 6 7 8
-9 9 9 9 2
-*/
