6c5172955362f13b385f6888006cf3d646e57114
Ashutosh Kale <ashutoshkale@outlook.com>
2021-08-31 16:18:07 -0700

    Time: 1 ms (54.05%), Space: 37.1 MB (83.59%) - LeetHub

--- alphabet-board-path/alphabet-board-path.java
+++ alphabet-board-path/alphabet-board-path.java
@@ -1,91 +1,93 @@
-class Point{
-    public int x;
-    public int y;
-    public Point(int x, int y){
+class Point {
+    int x;
+    int y;
+    
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
     
-    @Override
-    public int hashCode() {
-        return 7 * this.x * 13 * this.y;
-    }
-    
-    @Override
-    public boolean equals(Object other){
-        Point p = (Point) other;
-        return p.x == this.x && p.y == this.y;
-    }
-    
-    @Override
-    public String toString() {
-        return "x:" + x + ",y" + y;
-    }
 }
+
 class Solution {
     public String alphabetBoardPath(String target) {
-        Map<Character, Point> position = new HashMap<>();
         String[] board = new String[]{"abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"};
-        for(int i = 0; i < board.length; i++){
-            String word = board[i];
-            for(int j = 0; j < word.length(); j++) {
+        Map<Character, Point> positionMap = new HashMap<>();
+        for(int i = 0; i < board.length; i++) {
+            String currentWord = board[i];
+            for(int j = 0; j < currentWord.length(); j++) {
+                Character current = currentWord.charAt(j);
                 Point p = new Point(i, j);
-                position.put(word.charAt(j), p);
+                positionMap.put(current, p);
             }
         }
         
-        
+        Point previousPos = new Point(0,0);
         StringBuilder sb = new StringBuilder();
-        Character previous = 'a';
+        
         for(int i = 0; i < target.length(); i++) {
+            Character to = target.charAt(i);
+            Point toPos = positionMap.get(to);
+            int up = 0;
+            int down = 0;
+            int left = 0;
+            int right = 0;
+            
+            // Calculate Up
+            if(toPos.x < previousPos.x) {
+                up = previousPos.x - toPos.x;
+            }
+            
+            
+            // Calculate Left
+            if(toPos.y < previousPos.y) {
+                left = previousPos.y - toPos.y;
+            }
+            
+            
+            // Calculate Right
+            if(toPos.y > previousPos.y){
+                right = toPos.y - previousPos.y;
+            }
+            
+            
+            // Calculate Down
+            if(toPos.x > previousPos.x) {
+                down = toPos.x - previousPos.x;
+            }
            
-            Point previousPoint = position.get(previous);
             
-            Character current = target.charAt(i);
-            Point currentPoint = position.get(current);
             
-            /*
-            System.out.println(previous);
-            System.out.println(current);
-            */
+            previousPos = toPos;
             
-            int xDiff = Math.abs(previousPoint.x - currentPoint.x);
-            int yDiff = Math.abs(previousPoint.y - currentPoint.y);
-            
-            System.out.println();
-            
-            if(xDiff == 0 && yDiff == 0) {
-                sb.append("!");
-                previous = current;
-            } else {
-                
-                int down = currentPoint.x - previousPoint.x > 0 ? Math.abs(currentPoint.x - previousPoint.x) : 0;
-                int up = currentPoint.x - previousPoint.x < 0 ? Math.abs(currentPoint.x - previousPoint.x) : 0;
-                int left = currentPoint.y - previousPoint.y < 0 ? Math.abs(currentPoint.y - previousPoint.y) : 0;
-                int right = currentPoint.y - previousPoint.y > 0 ? Math.abs(currentPoint.y - previousPoint.y) : 0;
-                
-                for(int j = 0; j < up; j++){
+            if(up > 0) {
+                for(int j = 0; j < up; j++) {
                     sb.append("U");
                 }
-                
-                for(int j = 0; j < right; j++){
-                    sb.append("R");
-                }
-                
-                for(int j = 0; j < left; j++){
+            }
+            
+            if(left > 0) {
+                for(int j = 0; j < left; j++) {
                     sb.append("L");
                 }
-                
-                for(int j = 0; j < down; j++){
+            }
+            
+            if(right > 0) {
+                for(int j = 0; j < right; j++) {
+                    sb.append("R");
+                }
+            }
+            
+            
+            if(down > 0) {
+                for(int j = 0; j < down; j++) {
                     sb.append("D");
                 }
-                
-                sb.append("!");
-                previous = current;
-                
             }
+            
+            sb.append("!");  
         }
-        return sb.toString();
         
+        return sb.toString();
     }
 }
