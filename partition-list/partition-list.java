8f860a662f5d9b388cb1a28edc3aa7b3ba0ebe86
Ashutosh Kale <ashutoshkale@outlook.com>
2021-08-30 17:30:11 -0700

    Time: 1 ms (13.14%), Space: 39.4 MB (8.67%) - LeetHub

--- partition-list/partition-list.java
+++ partition-list/partition-list.java
@@ -10,38 +10,29 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head == null){
+        if(head == null || head.next == null) {
             return head;
         }
-        Queue<ListNode> less = new LinkedList<>();
-        Queue<ListNode> more = new LinkedList<>();
+        ListNode less = new ListNode(0);
+        ListNode more = new ListNode(0);
+        ListNode moreHead = more;
+        ListNode lessHead = less;
+        
         ListNode runner = head;
         while(runner != null) {
-            if(runner.val < x){
-                less.add(runner);
-                
-            } else if(runner.val >= x){
-                more.add(runner);
+            if(runner.val < x) {
+                less.next = new ListNode(runner.val);
+                less = less.next;
+            } else {
+                more.next = new ListNode(runner.val);
+                more = more.next;
             }
             runner = runner.next;
         }
         
-       
-        ListNode sol = new ListNode(-1);
-        ListNode solHead = sol;
-        while(!less.isEmpty()){
-            sol.next = (less.remove());
-            sol = sol.next;
-        }
+        less.next = moreHead.next;
+        return lessHead.next;
         
-        while(!more.isEmpty()){
-            sol.next = (more.remove());
-            sol = sol.next;
-        }
-        sol.next = null;
-        
-        return solHead.next;
-        
-        
+    
     }
 }
