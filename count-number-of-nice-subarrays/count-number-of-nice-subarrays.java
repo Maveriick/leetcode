a396b2de684f0dc1adb422559319ceb0b2f872e4
Ashutosh Kale <ashutoshkale@outlook.com>
2021-08-31 21:49:15 -0700

    Time: 229 ms (5.06%), Space: 115.1 MB (5.06%) - LeetHub

--- count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,34 +1,30 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        
-        
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 2 == 1){
-                nums[i] = 1;
-            } else {
-                nums[i] = 0;
-            }
-        }
-        
-        System.out.println(Arrays.toString(nums));
-        int[] prefixSum = new int[nums.length + 1];
-        prefixSum[0] = 0;
-        int currentSum = 0;
+        int[] oddNumbers = new int[nums.length + 1];
+        oddNumbers[0] = 0;
+        int totalOddNumbers = 0;
         for(int i = 0; i < nums.length; i++) {
-            currentSum += nums[i];
-            prefixSum[i+1] = currentSum;
+            if(nums[i] % 2 == 1){
+                totalOddNumbers++;
+            }
+            oddNumbers[i+1] = totalOddNumbers;
         }
         
-        int arrays = 0;
-        Map<Integer, Integer> frequency = new HashMap<>();
-        System.out.println(Arrays.toString(prefixSum));
-        for(int i = 0; i < prefixSum.length; i++) {
-                int required = prefixSum[i] - k;
-                if(frequency.containsKey(required)){
-                    arrays +=  frequency.get(required);
-                }
-                frequency.put(prefixSum[i], frequency.getOrDefault(prefixSum[i],0) + 1);
+        System.out.println(Arrays.toString(oddNumbers));
+        int niceSubArrays = 0;
+        Map<Integer, Integer> current = new HashMap<>();
+        for(int i = 0; i < oddNumbers.length; i++) {
+            int needed = oddNumbers[i] - k;
+            if(current.containsKey(needed)){
+                niceSubArrays += current.get(needed);
+            }
+            
+            current.put(oddNumbers[i], current.getOrDefault(oddNumbers[i], 0) + 1);
         }
-        return arrays;
+        return niceSubArrays;
     }
+    
 }
+
+
+
