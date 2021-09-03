dec5071d100446f76c79f4ef577d45de448d4251
Ashutosh Kale <ashutoshkale@outlook.com>
2021-09-03 13:49:32 -0700

    Time: 8 ms (29.32%), Space: 44.3 MB (5.54%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -1,64 +1,45 @@
 class Interval {
-    int start;
-    int end;
-    public Interval(int s, int e) {
-        this.start = s;
-        this.end = e;
-    }
+    int startTime;
+    int endTime;
     
-    @Override
-    public String toString() {
-        return "[" + Integer.toString(start) + "," + Integer.toString(end) + "]";
+    public Interval( int startTime, int endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
 
 class Solution {
-    PriorityQueue<Interval> pq = new PriorityQueue<>((a,b) -> a.start - b.start);
     public int[][] merge(int[][] intervals) {
-        if(intervals.length == 1){
+        if(intervals.length <= 1){
             return intervals;
         }
-        for(int i = 0; i < intervals.length; i++) {
-            int[] current = intervals[i];
-            Interval inter = new Interval(current[0], current[1]);
-            pq.add(inter);
-        }
         
-        List<Interval> sorted = new ArrayList<>();
-        while(!pq.isEmpty()){
-            sorted.add(pq.poll());
-        }
-        
-        List<Interval> sol = new ArrayList<>();
-        Interval previous = null;
-        int start = 0;
-        while(start < sorted.size()) {
-            if(previous == null){
-                previous = sorted.get(start);
-                start++;
+        Arrays.sort(intervals, ((a,b) -> a[0] - b[0]));
+        Interval previous = new Interval(intervals[0][0], intervals[0][1]);
+        List<Interval> merged = new ArrayList<>();
+        for(int i = 1; i < intervals.length; i++) {
+            if(previous == null) {
+                previous = new Interval(intervals[i][0], intervals[i][1]);
+            }else if(intervals[i][0] <= previous.endTime) {
+                previous.startTime = Math.min(previous.startTime, intervals[i][0]);
+                previous.endTime = Math.max(previous.endTime, intervals[i][1]);
             } else {
-                Interval current = sorted.get(start);
-                if(current.start <= previous.end){
-                    previous.start = Math.min(previous.start, current.start);
-                    previous.end = Math.max(previous.end, current.end);
-                } else {
-                    sol.add(previous);
-                    previous = current;
-                }
-                start++;
+                merged.add(previous);
+                previous = new Interval(intervals[i][0], intervals[i][1]);
             }
         }
-        sol.add(previous);
-        System.out.println(sol.toString());
+        
+        merged.add(previous);
         
         
-        int[][] solution = new int[sol.size()][2];
-        for(int i = 0; i < sol.size(); i++) {
-            int startTime = sol.get(i).start;
-            int endTime = sol.get(i).end;
-            solution[i][0] = startTime;
-            solution[i][1] = endTime;
+        int[][] toReturn = new int[merged.size()][2];
+        for(int i = 0; i < merged.size(); i++) {
+            Interval current = merged.get(i);
+            toReturn[i][0] = current.startTime;
+            toReturn[i][1] = current.endTime;
         }
-        return solution;
+        
+        return toReturn;
+                    
     }
 }
