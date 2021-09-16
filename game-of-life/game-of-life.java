82cf0acf3fb6a907a3a7ce2d8178d965a387c471
Ashutosh Kale <ashutoshkale@outlook.com>
2021-09-16 13:59:24 -0700

    Time: 1 ms (32.32%), Space: 39.1 MB (14.25%) - LeetHub

--- game-of-life/game-of-life.java
+++ game-of-life/game-of-life.java
@@ -1,47 +1,61 @@
 class Solution {
-     int[] xDirs = new int[]{-1,-1,-1,1,1,1,0,0};
-     int[] yDirs = new int[]{-1,0,1,-1,0,1,-1,1};
-    int[][] sol;
+    int[] xDirs = new int[]{-1,-1,-1,1,1,1,0,0};
+    int[] yDirs = new int[]{-1,0,1,-1,0,1,-1,1};
     public void gameOfLife(int[][] board) {
-       int rows = board.length;
+        int rows = board.length;
         int cols = board[0].length;
-        sol = new int[rows][cols];
+        getNextState(board, board.length, board[0].length);
         
         for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                int current = board[i][j];
+            for(int j = 0; j < cols; j++){
+                if(board[i][j] > 0) {
+                    board[i][j] = 1;
+                } else {
+                    board[i][j] = 0;
+                }
+            }
+        }
+    }
+    
+    private void getNextState(int[][] board, int rows, int cols) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                
                 int liveNeighbours = 0;
-               
                 for(int k = 0; k < 8; k++) {
                     int newX = i + xDirs[k];
                     int newY = j + yDirs[k];
                     if(newX >= 0 && newX < rows && newY >= 0 && newY < cols) {
-                        if(board[newX][newY] == 1){
-                            liveNeighbours++;
+                        if(Math.abs(board[newX][newY]) == 1) {
+                            liveNeighbours ++;
                         }
                     }
                 }
-               
-                if(current == 1){
+                
+                if(board[i][j] == 1) {
+                    // Cell is live
                     if(liveNeighbours < 2 || liveNeighbours > 3){
-                        sol[i][j] = 0;
-                    } else {
-                        sol[i][j] = 1;
-                    }
+                        board[i][j] = -1;
+                    } 
+                    
                 } else {
-                    if(liveNeighbours == 3) {
-                        sol[i][j] = 1;
-                    } else {
-                        sol[i][j] = 0;
-                    }
+                    // Cell is dead
+                   if(liveNeighbours == 3){
+                        board[i][j] = 2;
+                    } 
                 }
-            }
-        }
-        
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                board[i][j] = sol[i][j];
             }
         }
     }
 }
+
+/*
+[[0,1,0],
+[1,0,1],
+[0,1,1],
+[0,1,0]]
+
+
+*/
+
+
