4ed386c50a6f3b89b179656ecd24e2b96eb638fe
Ashutosh Kale <ashutoshkale@outlook.com>
2021-09-21 08:38:15 -0700

    Time: 241 ms (8.03%), Space: 39.9 MB (56.83%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,27 +1,31 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-         Set<Integer> indexesToRemove = new HashSet<>();
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
+        
+        if(s.length() == 0){
+            return s;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '('){
                 stack.push(i);
-            } if (s.charAt(i) == ')') {
-                if (stack.isEmpty()) {
-                    indexesToRemove.add(i);
-                } else {
+            } else if(s.charAt(i) == ')') {
+                if(!stack.isEmpty() && s.charAt(stack.peek()) =='(') {
                     stack.pop();
+                } else {
+                    stack.push(i);
                 }
             }
         }
-        // Put any indexes remaining on stack into the set.
-        while (!stack.isEmpty()) indexesToRemove.add(stack.pop());
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (!indexesToRemove.contains(i)) {
-                sb.append(s.charAt(i));
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(!stack.isEmpty() && stack.peek() == i){
+                stack.pop();
+            } else {
+               sb.insert(0, s.charAt(i));
             }
         }
-        return sb.toString();
         
+        return sb.toString();
     }
 }
