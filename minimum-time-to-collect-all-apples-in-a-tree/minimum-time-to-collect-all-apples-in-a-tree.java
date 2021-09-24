e7631c936c6b2a7da9a123e913b6b49e9f00d4e0
Ashutosh Kale <ashutoshkale@outlook.com>
2021-09-24 15:29:36 -0700

    Time: 77 ms (45.11%), Space: 80.3 MB (92.33%) - LeetHub

---
+++ minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
@@ -0,0 +1,44 @@
+class Solution {
+    Set<Integer> visited = new HashSet<>();
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+       Map<Integer, List<Integer>> graph = buildGraph(edges);
+       return dfs(graph, 0, 0, hasApple);    
+    }
+    
+    
+    private int dfs(Map<Integer, List<Integer>> graph, int currentVertex, int currentPathLength, List<Boolean> hasApple) {
+        visited.add(currentVertex);
+        List<Integer> neighbors = graph.getOrDefault(currentVertex, new ArrayList<>());
+        
+        int time = 0;
+        for(int i = 0; i < neighbors.size(); i++) {
+            if(!visited.contains(neighbors.get(i))){
+                time += dfs(graph, neighbors.get(i), currentPathLength + 1, hasApple);
+            }
+        }
+        
+        if (currentVertex != 0 && (hasApple.get(currentVertex) || time > 0))
+            time += 2;
+        
+        return time;
+    }
+    
+    
+    private Map<Integer, List<Integer>> buildGraph(int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i = 0; i < edges.length; i++) {
+           int from = edges[i][0];
+            int to = edges[i][1];
+            
+            List<Integer> fromList = graph.getOrDefault(from, new ArrayList<>());
+            fromList.add(to);
+            
+            List<Integer> toList = graph.getOrDefault(to, new ArrayList<>());
+            toList.add(from);
+            
+            graph.put(from, fromList);
+            graph.put(to, toList);
+        }
+        return graph;
+    }
+}
