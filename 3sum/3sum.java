64b7209dcee27a24e09d241490a6498162191776
Ashutosh Kale <ashutoshkale@outlook.com>
2021-09-29 22:08:26 -0700

    Time: 19 ms (82.82%), Space: 43.4 MB (51.87%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,51 +1,47 @@
 class Solution {
     List<List<Integer>> solution = new ArrayList<>();
     public List<List<Integer>> threeSum(int[] nums) {
-       Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        Arrays.sort(nums);
         for(int i = 0; i < nums.length; i++) {
-            if(i == 0 || nums[i-1] != nums[i]){
-                findSum(nums, 0 - nums[i], i);
+            int current = nums[i];
+            if(i == 0 || nums[i-1] != nums[i]){ 
+                 checkPairs(nums, i, 0 - current);
             }
-            
+           
         }
         return solution;
     }
     
     
-    
-    private void findSum(int[] nums, int target, int currentIndex) {
-        int start = currentIndex + 1;
+    private void checkPairs(int[] nums, int index, int target) {
+        int start = index + 1;
         int end = nums.length - 1;
-       
         
-        while(start < end) {
+        while( start < end){
             int sum = nums[start] + nums[end];
-            if(sum < target) {
-                start ++;
-            } else if(sum > target){
-                end --;
-            } else {
-                List<Integer> currentSolution = new ArrayList<>();
-                currentSolution.add(nums[currentIndex]);
-                if(sum == target){
-                    currentSolution.add(nums[start]);
-                    currentSolution.add(nums[end]);
-                    solution.add(currentSolution);
-                    
-                    int leftStart = start + 1;
-                    while(leftStart < end && nums[leftStart] == nums[start]){
-                        leftStart++;
-                    }
-                    start = leftStart;
-                    
-                    int rightStart = end - 1;
-                    while(rightStart > start && nums[rightStart] == nums[end]) {
-                        rightStart --;
-                    }
-                    end = rightStart;
+            if(sum == target) {
+                List<Integer> currentList = new ArrayList<>();
+                currentList.add(nums[index]);
+                currentList.add(nums[start]);
+                currentList.add(nums[end]);
+                solution.add(new ArrayList<>(currentList));
+                while(start < end && nums[start + 1] == nums[start]) {
+                    start ++;
                 }
+                start++;
+                
+                while(end > start && nums[end - 1] == nums[end]){
+                    end --;
+                } 
+                end--;
+               
+            } else if(sum < target){
+                start ++;
+            } else {
+                end --;
             }
         }
-    } 
+    }
+    
+    
 }
