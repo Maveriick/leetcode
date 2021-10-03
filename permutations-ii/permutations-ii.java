4719df1ee48c3d7b7cfd7b79023fef7995970531
Ashutosh Kale <ashutoshkale@outlook.com>
2021-10-03 12:37:15 -0700

    Time: 3 ms (55.90%), Space: 39.7 MB (68.25%) - LeetHub

--- permutations-ii/permutations-ii.java
+++ permutations-ii/permutations-ii.java
@@ -2,27 +2,26 @@ class Solution {
     List<List<Integer>> solution = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
         Arrays.sort(nums);
-        backTrack(nums, new ArrayList<>(), new boolean[nums.length]);
+        permute(nums, new ArrayList<>(), new HashSet<>());
         return solution;
     }
     
-    
-    private void backTrack(int[] nums, List<Integer> currentList, boolean[] visited) {
-        if(currentList.size() == nums.length) {
+    private void permute(int[] nums, List<Integer> currentList, Set<Integer> visited) {
+        if(currentList.size() == nums.length){
             solution.add(new ArrayList<>(currentList));
             return;
         }
         
         for(int i = 0; i < nums.length; i++) {
-            if(visited[i] == true || i > 0 && nums[i-1] == nums[i] && visited[i-1] == true){
+            if(visited.contains(i) || i > 0 && nums[i] == nums[i-1] && !visited.contains(i-1)){
                 continue;
             }
-        
             currentList.add(nums[i]);
-            visited[i] = true;
-            backTrack(nums, currentList, visited);
+            visited.add(i);
+            permute(nums, currentList, visited);
+            visited.remove(i);
             currentList.remove(currentList.size() - 1);
-            visited[i] = false;
+            
         }
     }
 }
