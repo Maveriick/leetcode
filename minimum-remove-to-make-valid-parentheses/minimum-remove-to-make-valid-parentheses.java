e58dcea6ab9c8bca35225cbecdc9012c86f9c93a
Ashutosh Kale <ashutoshkale@outlook.com>
2021-10-03 20:19:26 -0700

    Time: 460 ms (6.71%), Space: 49.5 MB (21.71%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,31 +1,34 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        
-        if(s.length() == 0){
+        if(s.length() == 0) {
             return s;
         }
+        
         Stack<Integer> stack = new Stack<>();
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == '('){
                 stack.push(i);
-            } else if(s.charAt(i) == ')') {
-                if(!stack.isEmpty() && s.charAt(stack.peek()) =='(') {
-                    stack.pop();
-                } else {
-                    stack.push(i);
+            } else {
+                if(s.charAt(i) == ')'){
+                    if(!stack.isEmpty() && s.charAt(stack.peek()) == '(') {
+                        stack.pop();
+                    } else {
+                        stack.push(i);
+                    }
                 }
             }
         }
+        
+       // System.out.println(stack.toString());
         
         StringBuilder sb = new StringBuilder();
         for(int i = s.length() - 1; i >= 0; i--) {
             if(!stack.isEmpty() && stack.peek() == i){
                 stack.pop();
             } else {
-               sb.insert(0, s.charAt(i));
+                sb.insert(0, s.charAt(i));
             }
         }
-        
         return sb.toString();
     }
 }
