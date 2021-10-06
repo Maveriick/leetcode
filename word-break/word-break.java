9d2c9507c2d1bc4648fb7e7fedc5752dd29bb1a5
Ashutosh Kale <ashutoshkale@outlook.com>
2021-10-05 17:41:27 -0700

    Time: 25 ms (5.18%), Space: 44.2 MB (11.10%) - LeetHub

--- word-break/word-break.java
+++ word-break/word-break.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(0);
         Set<Integer> visited = new HashSet<>();
-        Queue<Integer> bfsQ = new LinkedList<>();
-        bfsQ.add(0);
         
-        
-        while(!bfsQ.isEmpty()) {
-            int currentIndex = bfsQ.remove();
+        while(!pq.isEmpty()) {
+            int currentIndex = pq.poll();
             if(currentIndex == s.length()) {
                 return true;
-            } else {
-                for(int i = currentIndex + 1; i <= s.length(); i++) {
-                    if(wordDict.contains(s.substring(currentIndex, i))) {
-                        if(!visited.contains(i)) {
-                            visited.add(i);
-                            bfsQ.add(i);
-                        }
-                    }
+            }
+            for(int i = currentIndex; i <= s.length(); i++) {
+                if(visited.contains(i)){
+                    continue;
+                }
+                String possibleWord = s.substring(currentIndex, i);
+                if(wordDict.contains(possibleWord)) {
+                    visited.add(i);
+                    pq.add(i);
                 }
             }
         }
+        
         return false;
+        
     }
 }
