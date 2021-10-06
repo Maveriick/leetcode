2e279037be8b33855b1edb0055c80e1a7483afb2
Ashutosh Kale <ashutoshkale@outlook.com>
2021-10-05 18:11:53 -0700

    Time: 0 ms (100.00%), Space: 39 MB (78.08%) - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -1,31 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int mStart = m - 1;
-        int nStart = n - 1;
-        int mergeStart = m + n - 1;
+        int n1Start = m - 1;
+        int n2Start = n - 1;
+        int solutionStart = m + n - 1;
         
-        while(mStart >= 0 && nStart >= 0) {
-            if(nums1[mStart] >= nums2[nStart]) {
-                nums1[mergeStart] = nums1[mStart];
-                mStart --;
+        while(n1Start >= 0 && n2Start >= 0) {
+            if(nums1[n1Start] > nums2[n2Start]) {
+                nums1[solutionStart] = nums1[n1Start];
+                n1Start --;
             } else {
-                nums1[mergeStart] = nums2[nStart];
-                nStart --;
+                nums1[solutionStart] = nums2[n2Start];
+                n2Start --;
             }
-            mergeStart--;
+            solutionStart --;
         }
         
-        if(mStart < 0) {
-            while(nStart >= 0) {
-                nums1[mergeStart] = nums2[nStart];
-                nStart --;
-                mergeStart--;
+        if(n1Start < 0) {
+            while(n2Start >= 0) {
+                 nums1[solutionStart] = nums2[n2Start];
+                n2Start --;
+                solutionStart --;
             }
         } else {
-            while(mStart >= 0) {
-                nums1[mergeStart] = nums1[mStart];
-                mStart --;
-                mergeStart--;
+             while(n1Start >= 0) {
+                nums1[solutionStart] = nums1[n1Start];
+                n1Start --;
+                 solutionStart --;
             }
         }
     }
